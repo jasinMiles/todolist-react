fae3074ca35999a1e4c9e1cb61019acd8572ecf3
Michael Okeme <avwerosuokeme@gmail.com>
2023-07-16 22:28:01 +0100

    commit

--- src/GLOBAL/features/components/AddTodo.js
+++ src/GLOBAL/features/components/AddTodo.js
@@ -90,54 +90,31 @@ const AddTodo = ({ refetch }) => {
             name="title"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
-            className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="form-control form-control-lg"
           />
         </div>
         <div className="flex justify-center">
           <button
             type="submit"
-            className={`inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white 
-              ${
-                isLoading
-                  ? "bg-indigo-500 hover:bg-indigo-400 cursor-not-allowed"
-                  : "bg-indigo-700 hover:bg-indigo-600 cursor-pointer"
-              } transition ease-in-out duration-150 mr-4`}
-            disabled={isLoading}
+            id='submit-button'
           >
-            {isLoading && (
-              <svg
-                className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <circle
-                  className="opacity-25"
-                  cx="12"
-                  cy="12"
-                  r="10"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                ></circle>
-                <path
-                  className="opacity-75"
-                  fill="currentColor"
-                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                ></path>
-              </svg>
-            )}
+            
             {isLoading
               ? "Processing..."
               : isEditing
               ? "Update Todo"
-              : "Add Todo"}
+              : "Submit"}
           </button>
+          &nbsp;
+                  &nbsp;
+                  &nbsp;
+                  &nbsp;
           {isEditing && (
             <button
               onClick={newTodo}
-              className="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md bg-teal-400 text-white"
+              id='cancel-button'
             >
-              New Todo
+              Cancel
             </button>
           )}
         </div>
